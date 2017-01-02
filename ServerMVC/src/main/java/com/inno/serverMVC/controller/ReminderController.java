fc25dc9bf2515416acdd97ea1d8b2f8d73d60443
Rommel <dragondrum@mail.ru>
2017-01-02 21:55:09 +0300

    a

---
+++ ServerMVC/src/main/java/com/inno/serverMVC/controller/ReminderController.java
@@ -0,0 +1,21 @@
+package com.inno.serverMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Home on 16.12.2016.
+ */
+@Controller
+@RequestMapping("/reminder")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getReminder(ModelMap model) {
+        return "get Reminder";
+    }
+}
