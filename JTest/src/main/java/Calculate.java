3ca6f905e809f11bc99ad14928bf917243d949b8
Rommel <dragondrum@mail.ru>
2017-01-05 16:57:35 +0300

    calculate

---
+++ JTest/src/main/java/Calculate.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Home on 05.01.2017.
+ */
+public class Calculate {
+
+    public static void main(String[] arg) {
+        System.out.println("Calculate...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("Sum :" + summ);
+    }
+}
