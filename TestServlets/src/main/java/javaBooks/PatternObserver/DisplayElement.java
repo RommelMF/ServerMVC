21851497000677e948a5ed72674b9b4091c1c0e2
Rommel <dragondrum@mail.ru>
2017-02-13 21:06:49 +0300

    deleteTestServlet

--- TestServlets/src/main/java/javaBooks/PatternObserver/DisplayElement.java
+++
@@ -1,8 +0,0 @@
-package javaBooks.PatternObserver;
-
-/**
- * Created by Home on 31.01.2017.
- */
-public interface DisplayElement {
-    public void display();
-}
--- TestServlets/src/main/java/javaBooks/PatternObserver/Observer.java
+++
@@ -1,8 +0,0 @@
-package javaBooks.PatternObserver;
-
-/**
- * Created by Home on 31.01.2017.
- */
-public interface Observer {
-    public void update(float temp, float humidity, float pressure);
-}
